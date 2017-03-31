7bde7b8a85e1db6cff0362078bd49048275eb146
puneethprog9 <puneethreddyb@gmail.com>
2017-03-31 10:10:39 +0530

    diectory recipe folder

---
+++ cdir/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: cdir
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+directory "/etc/puneeth" do
+	action :create
+end
