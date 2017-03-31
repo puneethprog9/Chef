45fbd497a65635cdc2f290ded7d1811ea5c88308
puneethprog9 <puneethreddyb@gmail.com>
2017-03-31 10:14:06 +0530

    httpd package recipe folder

---
+++ dir/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: dir
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+service "httpd" do
+    action :restart
+	end
